f734530902ace755dc3848cfe9d488d67ce66ecf
James Spencer <me@jspenc.com>
2021-11-16 22:05:25 +0000

    logo added (#11)

--- libs/ui/shared-ui/src/lib/header/header.tsx
+++ libs/ui/shared-ui/src/lib/header/header.tsx
@@ -4,25 +4,41 @@ import {
   Stack,
   IconButton,
   IconFilledTwitter,
-  Link,
   styled,
 } from 'newskit';
+import Link from 'next/link';
+import { Logo } from '../logo/logo';
 
-const HeaderContainer = styled.header`margin-top:16px;`;
+const HeaderContainer = styled.header`
+  margin-top: 16px;
+`;
 
 export const Header = () => (
   <HeaderContainer>
     <Stack flow="horizontal-center" stackDistribution="space-between">
       <Block>
-        <Link href="/">James Spencer</Link>
+        <Link href="/" passHref>
+          {/* This href attribute isn't technically needed but gets around linter warnings because of next.js's weird api */}
+          <a href="/">
+            <Logo title="Go to James Spencer home" />
+          </a>
+        </Link>
       </Block>
       <Block>
         <Stack flow="horizontal-center" spaceInline="space030">
-          <IconButton aria-label="github">
-            <IconFilledGitHub overrides={{ size: 'iconSize020' }} />
+          <IconButton
+            aria-label="github"
+            overrides={{ stylePreset: 'iconButtonMinimalSecondary' }}
+            href="https://github.com/jps"
+          >
+            <IconFilledGitHub overrides={{ size: 'iconSize030' }} />
           </IconButton>
-          <IconButton aria-label="twitter">
-            <IconFilledTwitter overrides={{ size: 'iconSize020' }} />
+          <IconButton
+            aria-label="twitter"
+            overrides={{ stylePreset: 'iconButtonMinimalSecondary' }}
+            href="https://twitter.com/jpspenc"
+          >
+            <IconFilledTwitter overrides={{ size: 'iconSize030' }} />
           </IconButton>
         </Stack>
       </Block>
