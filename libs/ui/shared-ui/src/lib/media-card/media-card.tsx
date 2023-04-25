a733094acf56fc4a6660f0620395d1dcd793ecdf
James Spencer <me@jspenc.com>
2023-04-25 22:35:40 +0100

    remove obsolete ref since next upgrade

--- libs/ui/shared-ui/src/lib/media-card/media-card.tsx
+++ libs/ui/shared-ui/src/lib/media-card/media-card.tsx
@@ -1,6 +1,5 @@
 import { CardProps, Card, Headline, TextBlock, Stack } from 'newskit';
 import Link from 'next/link';
-import { forwardRef } from 'react';
 
 export interface MediaCardProps extends CardProps {
   headline: string;
@@ -12,20 +11,20 @@ type MediaCardPropsWithOptionalHref = Omit<MediaCardProps, 'href'> & {
   href?: string;
 };
 
-const CardWrapped = forwardRef<HTMLDivElement, MediaCardPropsWithOptionalHref>(
-  ({ children, headline, href }) => (
-    <Card
-      layout="vertical"
-      href={href ? { href: href, target: '_blank' } : undefined}
-    >
-      <Stack flow="vertical-left" spaceInline="space040">
-        <Headline headingAs="h3">{headline}</Headline>
-        <TextBlock typographyPreset="editorialParagraph020">
-          {children}
-        </TextBlock>
-      </Stack>
-    </Card>
-  )
+const CardWrapped = ({
+  children,
+  headline,
+  href,
+}: MediaCardPropsWithOptionalHref) => (
+  <Card
+    layout="vertical"
+    href={href ? { href: href, target: '_blank' } : undefined}
+  >
+    <Stack flow="vertical-left" spaceInline="space040">
+      <Headline headingAs="h3">{headline}</Headline>
+      <TextBlock typographyPreset="editorialParagraph020">{children}</TextBlock>
+    </Stack>
+  </Card>
 );
 
 export const MediaCard = ({ href, children, headline }: MediaCardProps) => (
